27dfb1249ee70756685077fddd8cf019618ad4ea
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2024-03-17 22:33:12 -0400

    erros in aprs tx

--- tracker_ahe/tracker_ahe.ino
+++ tracker_ahe/tracker_ahe.ino
@@ -51,11 +51,17 @@ char cmd[20];
 int cmd_cnt = 0;
 
 int time_share = 0;
-int msg_id = 0;
+int msg_id = -1;
+int msg = 0;
 int msg_valid = 0;
 char myCALL[] = "KE8TJE";
 float freq_tx;
 
+// v4.2
+char test_data[100];
+char raw[100];
+char gps_cmd[8];
+char *data = test_data;
 int packet_id = 0;
 
 void setup() {
@@ -63,9 +69,11 @@ void setup() {
   Serial.begin(57600);  // for logging
   //Wire.begin(0x08);     //added for i2c`:w
 
-   
+
   Serial.println("[info] KE8TJE - APRS tracker v4 - Beta");
   Serial.println("[info] IO init");
+ 
+
   dra_serial = new SoftwareSerial(RX, TX);  // Instantiate the Software Serial Object.
 
   init_radio();
@@ -82,7 +90,13 @@ void setup() {
   Serial.print("[info] ");
   Serial.println(freq_tx);
   //start GPS
-  gps.begin(9600);
+
+
+  if (digitalRead(sim_packet) == 0) {
+    Serial.print("[info] Sim packet");
+  }
+
+ 
 
   //init radio module and change frequency
 
@@ -94,84 +108,73 @@ void setup() {
   } else {
     Serial.println("[info] RF OK");
   }
+  delay(1000);
+  gps.begin(9600);
 }
 
 void loop() {
-
-
-  gps.stopListening();
-  Wire.end(); //ahe-1
   
-  if (msg_id > 0 & msg_valid == 1) {
+  
+  gps.stopListening();
+  Wire.end();  //ahe-1
+
+  // if (msg_id > 10 & msg_valid == 1) {
+  if ( msg_valid == 1) {
+    gps.flush();
     location_update();
     Serial.print("APRS:");
     Serial.println(freq_tx);
   } else {
+    Serial.print(msg_id);
+    Serial.print(",");
+    Serial.print(msg);
+    Serial.print(",");
+    Serial.println(msg_valid);
     Serial.println("[info] ------------------------ No location data");
   }
   gps.listen();
-  Wire.begin(0x08); //ahe-1
-  Wire.onReceive(receiveEvent); // ahe-1
-  
+  Wire.begin(0x08);              //ahe-1
+  Wire.onReceive(receiveEvent);  // ahe-1
+
 
   while (time_share < timeout) {
     while (gps.available() > 0) {
       time_share += 1;
-      String gps_raw = gps.readStringUntil('\n');
+      //String gps_raw = gps.readStringUntil('\n');
+      gps.readBytesUntil('\n', data, 100);
 
-      // Valid data: $GPGLL,3938.28486,N,07957.13511,W,191757.00,A,A*7D
-      if (gps_raw.substring(0, 6) == "$GNRMC") {
-        //simulate locked data
-        if (digitalRead(sim_packet) == 0) {
-          //gps_raw = "$GPGLL,3938.28486,N,07957.13511,W,191757.00,A,A*7D";
+      //make a copy for simulation
+      strcpy(raw, test_data);
 
-          // V2 - Packet
-          // $GPGLL,3927.83254,N,0808.25462,W,130448.00,A,A*71
+      char *p = strtok(test_data, ",");  //code - <0>
+      sprintf(gps_cmd, "%s", p);
 
-          // V3 - Packet
-          // $GNRMC,134055.000,A,3509.7572,N,09010.4938,W,1.51,338.00,121023,,,A*6C
-          Serial.println("[info] Sim Packet");
+
+      if (digitalRead(sim_packet) == 0) {
+        //simulation mode
+        if (strcmp(gps_cmd, "$GNGGA") == 0) {
+          strcpy(raw, "$GNGGA,042303.000,3938.7688,N,07958.4412,W,1,09,1.33,328.5,M,-33.0,M,,*4F");
         }
-
-        //Serial.println(gps_raw);
-
-
-        if (gps_raw.length() > 30) {
-          msg_id++;
-          // GPS locked
-          //update_GPS(gps_raw);
-
-          //i2c functions needed
-        }
+        //Serial.println(raw);
       }
 
-      //long altitude packet - code
-      // v2 - "$GPGGA"
-      // v3 - "$GNGGA"
-      //Serial.println(gps_raw); 
 
-      // debug packet
-      //gps_raw = "$GNGGA,165006.000,2241.9107,N,12017.2383,E,1,14,0.79,22.6,M,18.5,M,,*42";
+      p = strtok(raw, ",");  //code - <0>
+      sprintf(gps_cmd, "%s", p);
 
-      //$GNGGA,033145.000,3938.0803,N,07957.1891,W,1,09,1.05,293.2,M,-33.0,M,,*4D
-
-      if (gps_raw.substring(0, 6) == "$GNGGA"){
-        
-        if (gps_raw.length() > 50) {
-          // GPS locked
-          //update_GPS(gps_raw);
-          Serial.println(gps_raw);
-          update_GPS_alt(gps_raw);
-          //i2c functions needed
-        }     
+      if (strcmp(gps_cmd, "$GNGGA") == 0) {
+        //msg_id ++;
+        //Serial.println("alt packet");
+        Serial.println(msg++);
+        update_GPS_alt(p);
+        //msg_id++;
       }
-
     }
   }
   time_share = 0;
 }
 
-void update_GPS(String gps_data) {
+int update_GPS(String gps_data) {
 
 
   char test_data[100];
@@ -184,13 +187,13 @@ void update_GPS(String gps_data) {
   // $GNRMC,134055.000,A,3509.7572,N,09010.4938,W,1.51,338.00,121023,,,A*6C
 
   char *p = strtok(test_data, ",");  //code
-  p = strtok(NULL,","); //time
-  p = strtok(NULL,","); //validifty
+  p = strtok(NULL, ",");             //time
+  p = strtok(NULL, ",");             //validifty
 
   //Serial.print("Data validity:");
   //Serial.println(p);
 
-  if (p[0]=='A') {
+  if (p[0] == 'A') {
     Serial.println("[i] GPS valid");
   } else {
     Serial.println("[!] Invalid data");
@@ -231,31 +234,28 @@ void update_GPS(String gps_data) {
   //Serial.println("Short packet:");
   Serial.println(Lat);
   Serial.println(Lon);
-  
-  
+
+
   msg_valid = 1;
 }
 
-void update_GPS_alt(String gps_data) {
+
+
+void update_GPS_alt(char *p) {
 
   //data is sent with 2 decimal places
   // reformat the data to be used by the APRS library
   //"$GPGGA,191757.00,3938.28486,N,07957.13511,W,1,03,2.71,274.5,M,-33.9,M,,*6F";
   // v3
   // $GNGGA,165006.000,2241.9107,N,12017.2383,E,1,14,0.79,22.6,M,18.5,M,,*42
-
   // Documentation: https://openrtk.readthedocs.io/en/latest/communication_port/nmea.html
   //$GNGGA<0>,000520.095<1>,<2>,<3>,<4>,<5>,0<6>,0<7>,<8>,<9>,M<10>,<11>,M<12>,<13>,*5D<14>
 
-  char test_data[100];
-  gps_data.toCharArray(test_data, 100);
+  p = strtok(NULL, ",");  //time - <1>
 
-  char *p = strtok(test_data, ",");  //code - <0>
-  p = strtok(NULL, ",");             //time - <1>
-
-
-  p = strtok(NULL, ",");             //lat - <2>
+  p = strtok(NULL, ",");  //lat - <2>
   sprintf(Lat, "%s", p);
+
 
   // bug fix in v2
   if (Lat[4] == '.') {
@@ -264,10 +264,9 @@ void update_GPS_alt(String gps_data) {
     Lat[8] = '\0';
   }
 
-  
+
   p = strtok(NULL, ",");  // lat_char - <3>
   sprintf(Lat, "%s%s\0", Lat, p);
-
   //p = strtok(NULL, ",");             //lng
   //p = strtok(NULL, ",");             //dir
 
@@ -284,32 +283,34 @@ void update_GPS_alt(String gps_data) {
   sprintf(Lon, "%s%s\0", Lon, p);
 
   p = strtok(NULL, ",");  //state - <6>
-  //Serial.print("-");
-  //Serial.println(p);
 
-  if (p[0]>='1' & p[0]<='4') {
-    ;
-  }
-  else{
-    //Serial.println("[!] Invalid data");
+
+  if (p[0] >= '1' & p[0] <= '4') {
+    // msg_id++;
+  } else {
+    Serial.println("[!] Invalid data");
     msg_valid = 0;
     return;
   }
 
-  sprintf(alt,"APRS-v4.0,");
-  
-  p = strtok(NULL, ",");    //sta-no - <7>
-  strcat(alt,p);
-  strcat(alt,",");
-  p = strtok(NULL, ",");    //horizontal 
-  strcat(alt,p);
-  strcat(alt,",alt=");
-  p = strtok(NULL, ",");    //alti
-  strcat(alt,p);
-  p = strtok(NULL, ",");    //alti-unit
-  strcat(alt,p);
-  
+  sprintf(alt, "APRS-v4.0,");
 
+  p = strtok(NULL, ",");  //sta-no - <7>
+  strcat(alt, p);
+  strcat(alt, ",");
+  p = strtok(NULL, ",");  //horizontal
+  strcat(alt, p);
+  strcat(alt, ",alt=");
+  p = strtok(NULL, ",");  //alti
+  strcat(alt, p);
+  p = strtok(NULL, ",");  //alti-unit
+  strcat(alt, p);
+  
+  Serial.print(Lat);
+  Serial.print(",");
+  Serial.println(Lon);
+
+  //msg_id++;
   msg_valid = 1;
 }
 
@@ -328,7 +329,7 @@ int location_update() {
     }
   }
   */
-  
+
   //radio_TX();
   Serial.println("[info] APRS:start");
   time_share = 0;
@@ -353,7 +354,7 @@ int location_update() {
   // $ - phone
   // [ - walker
 
-  APRS_setSymbol('>');  
+  APRS_setSymbol('>');
 
 
   char comment[30];
@@ -361,7 +362,7 @@ int location_update() {
   //sprintf(comment, "NEBP-WV msg_id:%d", msg_id);
   //APRS_sendLoc(comment, strlen(comment), ' ');
 
-  sprintf(alt,"%s,%d",alt,packet_id++);
+  sprintf(alt, "%s,%d", alt, packet_id++);
   APRS_sendLoc(alt, strlen(alt), ' ');
 
   delay(1200);
@@ -379,10 +380,9 @@ void receiveEvent(int howMany) {
     Serial.print(c);         // print the character
   }*/
   //cmd[cmd_cnt++];
-  int x = Wire.read();    // receive byte as an integer
-  Serial.print(x);         // print the integer
+  int x = Wire.read();  // receive byte as an integer
+  Serial.print(x);      // print the integer
   Serial.print(',');
-  if(cmd==12)
+  if (cmd == 12)
     Serial.println();
-
 }
