137882c47b0b59856d08d1ecbcceec4a1f0469b5
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2025-03-22 19:39:05 -0400

    updated the readme and cleaned unused code

--- tracker_ahe/README.md
+++ tracker_ahe/README.md
@@ -1,5 +1,9 @@
-## avrprogramming
+# APRS Tracker - customization 
 
-To program the APRS board use the following 
+Most common settings are set up as `#define` statements for easy configuration. Some of the major settings are listed below with their typical value
+
+## Programming the firmware
+
+You need to have avrdude installed on your local computer along with an USBasp programmer. To program the APRS board use the following command. 
 
 `avrdude -p m328p -c usbasp -U flash:w:tracker_ahe.ino.hex`
--- tracker_ahe/tracker_ahe.ino
+++ tracker_ahe/tracker_ahe.ino
@@ -211,7 +211,6 @@ void update_GPS_v2(char *p) {
   alt_num = atof(p);
 
  
-
   if (sec == APRS_TX_offset_utc_sec & (min % APRS_interval_count) == 0 & msg_valid) {
     Serial.println("[i] Event trigger");
     gps.stopListening();
@@ -221,107 +220,14 @@ void update_GPS_v2(char *p) {
     gps.listen();
   }
 
-
-
 }
 
-void update_GPS_alt(char *p) {
-
-  //data is sent with 2 decimal places
-  // reformat the data to be used by the APRS library
-  //"$GPGGA,191757.00,3938.28486,N,07957.13511,W,1,03,2.71,274.5,M,-33.9,M,,*6F";
-  // v3
-  // $GNGGA,165006.000,2241.9107,N,12017.2383,E,1,14,0.79,22.6,M,18.5,M,,*42
-
-  // Documentation: https://openrtk.readthedocs.io/en/latest/communication_port/nmea.html
-  //$GNGGA<0>,000520.095<1>,<2>,<3>,<4>,<5>,0<6>,0<7>,<8>,<9>,M<10>,<11>,M<12>,<13>,*5D<14>
-
-  //char *p = strtok(test_data, ",");  //code - <0>
-  p = strtok(NULL, ",");  //time - <1>
-
-
-  p = strtok(NULL, ",");  //lat - <2>
-  sprintf(Lat, "%s", p);
-
-  // bug fix in v2
-  if (Lat[4] == '.') {
-    Lat[7] = '\0';
-  } else {
-    Lat[8] = '\0';
-  }
-
-
-  p = strtok(NULL, ",");  // lat_char - <3>
-  sprintf(Lat, "%s%s\0", Lat, p);
-
-  //p = strtok(NULL, ",");             //lng
-  //p = strtok(NULL, ",");             //dir
-
-  p = strtok(NULL, ",");  //lng - <4>
-  sprintf(Lon, "%s", p);
-
-  if (Lon[4] == '.') {
-    Lon[7] = '\0';
-  } else {
-    Lon[8] = '\0';
-  }
-
-  p = strtok(NULL, ",");  //dir - <5>
-  sprintf(Lon, "%s%s\0", Lon, p);
-
-  p = strtok(NULL, ",");  //state - <6>
-  //Serial.print("-");
-  //Serial.println(p);
-
-  if (p[0] >= '1' & p[0] <= '4') {
-    ;
-  } else {
-    //Serial.println("[!] Invalid data");
-    msg_valid = 0;
-    return;
-  }
-
-  // add firmware version to the packet
-  sprintf(alt, "%x,", firmware_v);
-
-  p = strtok(NULL, ",");  //sta-no - <7>
-  strcat(alt, p);
-  strcat(alt, ",");
-  p = strtok(NULL, ",");  //horizontal
-  strcat(alt, p);
-  strcat(alt, ",alt=");
-  p = strtok(NULL, ",");  //alti
-  alt_num = atof(p);      //added altitude conversion for conditiona statements
-  strcat(alt, p);
-  p = strtok(NULL, ",");  //alti-unit
-  strcat(alt, p);
-
-
-  //Serial.print("alt len:");
-  //strcpy(alt,final_msg);
-  msg_valid = 1;
-
-  Serial.print(Lat);
-  Serial.print(",");
-  Serial.println(Lon);
-
-  if (packet_id % alt_freq_delay == 0 && radio_freq_change) {
-    dra->group(DRA818_12K5, freq_alt, freq_alt, 13, 4, 0);
-    Serial.println("alt_freq");
-    radio_freq_change = 0;
-  }
-
-  if (packet_id % alt_freq_delay == 1 && radio_freq_change) {
-    dra->group(DRA818_12K5, 144.390, 144.390, 13, 4, 0);
-    Serial.println("main_freq");
-    radio_freq_change = 0;
-  }
-}
 
 int location_update() {
 
   // stop transmitting on a busy channel
 
+  // setting radio to low power for high altitude
   if (alt_num > 7000) {
     APRS_setSymbol(aprs_icon_low);
     set_radio_pwr(0);
